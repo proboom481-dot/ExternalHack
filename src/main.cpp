d6246b5052615fb7f6fd0feb51853937e8c08c13
proboom481-dot <proboom481@gmail.com>
2026-09-18 21:26:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,125 +5,110 @@ using namespace geode::prelude;
 
 class ExternalHackMenu : public FLAlertLayer {
 protected:
-    bool m_noclip = false;
-    bool m_speedhack = false;
-    bool m_noParticles = false;
-    bool m_noShake = false;
-    bool m_noTrail = false;
-
-    CCMenu* m_menu = nullptr;
-
     bool init() {
         if (!FLAlertLayer::init(
             nullptr,
             "ExternalHack",
+            "ExternalHack menu",
             "CLOSE",
             nullptr,
-            nullptr
+            420.f,
+            false,
+            220.f,
+            1.0f
         )) {
             return false;
         }
 
-        this->setTouchEnabled(true);
-
         auto winSize = CCDirector::sharedDirector()->getWinSize();
 
-        auto title = CCLabelBMFont::create(
-            "ExternalHack",
-            "goldFont.fnt"
+        auto menu = CCMenu::create();
+        menu->setPosition(0, 0);
+        this->m_mainLayer->addChild(menu);
+
+        auto noclip = CCMenuItemSpriteExtra::create(
+            ButtonSprite::create("NOCLIP"),
+            this,
+            menu_selector(ExternalHackMenu::onNoclip)
         );
 
-        title->setPosition(
-            winSize.width / 2,
-            winSize.height - 35.f
+        noclip->setPosition(
+            winSize.width / 2.f - 100.f,
+            winSize.height / 2.f + 35.f
         );
 
-        title->setScale(0.8f);
-        this->m_mainLayer->addChild(title);
+        menu->addChild(noclip);
 
-        m_menu = CCMenu::create();
-        m_menu->setPosition(0, 0);
-        this->m_mainLayer->addChild(m_menu);
-
-        createButton("NOCLIP", 0, 90.f);
-        createButton("SPEEDHACK", 1, 40.f);
-        createButton("NO PARTICLES", 2, -10.f);
-        createButton("NO SHAKE", 3, -60.f);
-        createButton("NO TRAIL", 4, -110.f);
-
-        auto info = CCLabelBMFont::create(
-            "ExternalHack Android",
-            "chatFont.fnt"
+        auto speedhack = CCMenuItemSpriteExtra::create(
+            ButtonSprite::create("SPEEDHACK"),
+            this,
+            menu_selector(ExternalHackMenu::onSpeedhack)
         );
 
-        info->setPosition(
-            winSize.width / 2,
-            25.f
+        speedhack->setPosition(
+            winSize.width / 2.f + 100.f,
+            winSize.height / 2.f + 35.f
         );
 
-        info->setScale(0.6f);
-        this->m_mainLayer->addChild(info);
+        menu->addChild(speedhack);
+
+        auto particles = CCMenuItemSpriteExtra::create(
+            ButtonSprite::create("NO PARTICLES"),
+            this,
+            menu_selector(ExternalHackMenu::onParticles)
+        );
+
+        particles->setPosition(
+            winSize.width / 2.f - 100.f,
+            winSize.height / 2.f - 25.f
+        );
+
+        menu->addChild(particles);
+
+        auto shake = CCMenuItemSpriteExtra::create(
+            ButtonSprite::create("NO SHAKE"),
+            this,
+            menu_selector(ExternalHackMenu::onShake)
+        );
+
+        shake->setPosition(
+            winSize.width / 2.f + 100.f,
+            winSize.height / 2.f - 25.f
+        );
+
+        menu->addChild(shake);
 
         return true;
     }
 
-    void createButton(const char* text, int id, float y) {
-        auto sprite = ButtonSprite::create(text);
-
-        auto button = CCMenuItemSpriteExtra::create(
-            sprite,
-            this,
-            menu_selector(ExternalHackMenu::onButton)
-        );
-
-        button->setTag(id);
-
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
-
-        button->setPosition(
-            winSize.width / 2,
-            winSize.height / 2 + y
-        );
-
-        m_menu->addChild(button);
-    }
-
-    void onButton(CCObject* sender) {
-        auto button = static_cast<CCMenuItemSpriteExtra*>(sender);
-        int id = button->getTag();
-
-        switch (id) {
-            case 0:
-                m_noclip = !m_noclip;
-                showState("Noclip", m_noclip);
-                break;
-
-            case 1:
-                m_speedhack = !m_speedhack;
-                showState("Speedhack", m_speedhack);
-                break;
-
-            case 2:
-                m_noParticles = !m_noParticles;
-                showState("No Particles", m_noParticles);
-                break;
-
-            case 3:
-                m_noShake = !m_noShake;
-                showState("No Shake", m_noShake);
-                break;
-
-            case 4:
-                m_noTrail = !m_noTrail;
-                showState("No Trail", m_noTrail);
-                break;
-        }
-    }
-
-    void showState(const char* name, bool enabled) {
+    void onNoclip(CCObject*) {
         FLAlertLayer::create(
-            name,
-            enabled ? "Enabled" : "Disabled",
+            "ExternalHack",
+            "Noclip: test button",
+            "OK"
+        )->show();
+    }
+
+    void onSpeedhack(CCObject*) {
+        FLAlertLayer::create(
+            "ExternalHack",
+            "Speedhack: test button",
+            "OK"
+        )->show();
+    }
+
+    void onParticles(CCObject*) {
+        FLAlertLayer::create(
+            "ExternalHack",
+            "No Particles: test button",
+            "OK"
+        )->show();
+    }
+
+    void onShake(CCObject*) {
+        FLAlertLayer::create(
+            "ExternalHack",
+            "No Shake: test button",
             "OK"
         )->show();
     }
@@ -149,15 +134,10 @@ class $modify(ExternalHackPauseLayer, PauseLayer) {
 
         auto menu = CCMenu::create();
         menu->setPosition(0, 0);
-
         this->addChild(menu, 100);
 
-        auto buttonSprite = ButtonSprite::create(
-            "EXTERNAL"
-        );
-
         auto button = CCMenuItemSpriteExtra::create(
-            buttonSprite,
+            ButtonSprite::create("EXTERNAL"),
             this,
             menu_selector(ExternalHackPauseLayer::onExternalHack)
         );
@@ -175,8 +155,7 @@ class $modify(ExternalHackPauseLayer, PauseLayer) {
     void onExternalHack(CCObject*) {
         auto popup = ExternalHackMenu::create();
 
-        if (popup) {
+        if (popup)
             popup->show();
-        }
     }
 };
